12625a60fc415e232573c5a215f2a31fc9ce0630
samir <samirbrahma2003@gmail.com>
2025-04-26 00:11:44 +0530

    commit
    added all files to the ripo

---
+++ allitems/script.js
@@ -0,0 +1,8 @@
+let button = document.querySelector('.btn4');
+
+function live() {
+    window.location.replace("../login/login.html");
+}
+
+// Attach the event listener to the button
+button.addEventListener('click', live);
